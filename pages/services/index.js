2bdf424ed2e0c18527702dcf5bf9f3e9b213aa30
Shiham123 <s36651786@gmail.com>
2023-12-28 21:32:51 +0600

    pages setup

---
+++ pages/services/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import {
+  RxCrop,
+  RxDesktop,
+  RxPencil2,
+  RxReader,
+  RxRocket,
+} from 'react-icons/rx';
+
+export const servicesData = [
+  {
+    icon: <RxCrop />,
+    title: 'Branding',
+    description: 'this is branding from services',
+  },
+  {
+    icon: <RxPencil2 />,
+    title: 'Design',
+    description: 'this is design from services',
+  },
+  {
+    icon: <RxDesktop />,
+    title: 'Development',
+    description: 'this is development from services',
+  },
+  {
+    icon: <RxReader />,
+    title: 'Copywriting',
+    description: 'This is copy writing from services',
+  },
+  {
+    icon: <RxRocket />,
+    title: 'SEO',
+    description: 'this is SEO from services',
+  },
+];
+
+const Services = () => {
+  return <div></div>;
+};
+
+export default Services;
---
+++ pages/work/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Work = () => {
+  return <div>this is work</div>;
+};
+
+export default Work;
