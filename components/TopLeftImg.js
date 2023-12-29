ec35ac95cbee15159962f1f9539049b3448812f4
Shiham123 <s36651786@gmail.com>
2023-12-29 10:37:39 +0600

    top left img added

--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -4,7 +4,7 @@ import topLeftImg from '../public/top-left-img.png';
 
 const TopLeftImg = () => {
   return (
-    <div className="absolute left-0 top-0 mix-blend-color-dodge z-10 w-[200px] xl:[400px]">
+    <div className="absolute left-0 top-0 mix-blend-color-dodge z-10 w-[200px] xl:[400px] opacity-50">
       <Image src={topLeftImg} width={400} height={400} alt="top-left-img" />
     </div>
   );
