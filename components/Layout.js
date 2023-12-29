1a6272077fdada271d3d12a45830777f8f29e9b8
Shiham123 <s36651786@gmail.com>
2023-12-29 10:31:02 +0600

    top left img added

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Sora } from '@next/font/google';
+import Nav from './Nav';
+import TopLeftImg from './TopLeftImg';
+import Header from './Header';
 
-const Layout = () => {
-  return <div>Layout</div>;
+const soraFont = Sora({
+  subsets: ['latin'],
+  variable: '--font-sora',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
+});
+
+const Layout = ({ children }) => {
+  return (
+    <div
+      className={`page bg-site text-white bg-cover bg-no-repeat ${soraFont.variable} font-sora relative`}
+    >
+      <TopLeftImg />
+      <Nav />
+      <Header />
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Image from 'next/image';
+import topLeftImg from '../public/top-left-img.png';
 
 const TopLeftImg = () => {
-  return <div>img one</div>;
+  return (
+    <div className="absolute left-0 top-0 mix-blend-color-dodge z-10 w-[200px] xl:[400px]">
+      <Image src={topLeftImg} width={400} height={400} alt="top-left-img" />
+    </div>
+  );
 };
 
 export default TopLeftImg;
