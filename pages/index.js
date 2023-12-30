0c41598b40724eaaaa5acbb5a1a32b0109ac75a0
Shiham123 <s36651786@gmail.com>
2023-12-30 19:13:33 +0600

    react-tsparticles and tsparticles version change from current to ^2.9.3 then particles work

--- pages/index.js
+++ pages/index.js
@@ -57,25 +57,27 @@ const Home = () => {
           </motion.div>
         </div>
       </div>
+
       {/* img */}
       <div className="w-[1200px] h-full absolute right-0 bottom-0">
         {/* explosion img */}
-        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0">
-          {/* Particles */}
-          <div>Particles</div>
-          {/* Avater img */}
-          {/* TODO: here create some img issue not able to fix it */}
-          <motion.div
-            variants={fadeIn('up', 0.5)}
-            initial="hidden"
-            animate="show"
-            exit="hidden"
-            transition={{ duration: 1, ease: easeInOut }}
-            className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]"
-          >
-            <Avatar />
-          </motion.div>
+        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
+        {/* Particles */}
+        <div>
+          <ParticlesContainer />
         </div>
+
+        {/* Avater img */}
+        <motion.div
+          variants={fadeIn('up', 0.5)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          transition={{ duration: 1, ease: easeInOut }}
+          className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]"
+        >
+          <Avatar />
+        </motion.div>
       </div>
     </div>
   );
