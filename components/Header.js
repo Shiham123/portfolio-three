bb5bf21639fc9fbb998f77a059add05db586c05a
Shiham123 <s36651786@gmail.com>
2023-12-31 09:44:20 +0600

    home page done with navblinks

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import Link from 'next/link';
+
+// component
+import Social from './Social';
 
 const Header = () => {
-  return <header className="absolute z-30 w-full">Header</header>;
+  return (
+    <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
+          {/* logo */}
+          <Link href={'/'}>
+            <h1 className="font-poppins italic text-2xl font-bold">
+              Shiham <span className="text-accent">portfolio</span>
+            </h1>
+          </Link>
+          {/* Socials */}
+          <div>
+            <Social />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- components/Social.js
+++ components/Social.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import Link from 'next/link';
+
+import { RiFacebookLine, RiGithubLine, RiGoogleLine, RiInstagramLine, RiLinkedinLine, RiTwitterLine, RiYoutubeLine } from 'react-icons/ri';
 
 const Social = () => {
-  return <div>this is social</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link href={'/'} className="hover:text-accent transition-all duration-300">
+        <RiYoutubeLine size={30} />
+      </Link>
+      <Link href={'/'} className="hover:text-accent transition-all duration-300">
+        <RiInstagramLine size={30} />
+      </Link>
+      <Link href={'/'} className="hover:text-accent transition-all duration-300">
+        <RiFacebookLine size={30} />
+      </Link>
+      <Link href={'/'} className="hover:text-accent transition-all duration-300">
+        <RiGithubLine size={30} />
+      </Link>
+      <Link href={'/'} className="hover:text-accent transition-all duration-300">
+        <RiGoogleLine size={30} />
+      </Link>
+      <Link href={'/'} className="hover:text-accent transition-all duration-300">
+        <RiTwitterLine size={30} />
+      </Link>
+      <Link href={'/'} className="hover:text-accent transition-all duration-300">
+        <RiLinkedinLine size={30} />
+      </Link>
+    </div>
+  );
 };
 
 export default Social;
