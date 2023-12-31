37e5d439e6efe37a5907cce276bbfe11d50d979d
Shiham123 <s36651786@gmail.com>
2023-12-31 20:45:26 +0600

    motion div added left side div

--- pages/about/index.js
+++ pages/about/index.js
@@ -26,20 +26,36 @@ const About = () => {
       >
         <Avatar />
       </motion.div>
+
+      {/* ----------------------- */}
       {/* body div */}
       <motion.div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
         {/* text or left section */}
+
         <div className="flex-1 flex flex-col justify-center">
-          <h2 className="h2">
+          <motion.h2 variants={fadeIn('right', 0.4)} initial="hidden" animate="show" exit="hidden" className="h2">
             Captivating <span className="text-accent">Stories</span>birth magnificent designs
-          </h2>
-          <p className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0">
+          </motion.h2>
+          <motion.p
+            variants={fadeIn('right', 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0"
+          >
             10 years ago, i began freelancing as a developer, Since then, i&apos;ve done remote work for agencies, consulted for startups, and
             collaborated on digital products for business and consumer use
-          </p>
+          </motion.p>
+
           {/* counters */}
-          <div>
-            <div>
+          <motion.div
+            variants={fadeIn('right', 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:mx-0 mb-8"
+          >
+            <div className="flex flex-1 xl:gap-x-5">
               {/* Experience */}
               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
                 <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
@@ -47,13 +63,24 @@ const About = () => {
                 </div>
                 <div className="text-xs uppercase tracking-[1px] leading-[1.5] max-w-[100px]">years of experience</div>
               </div>
+
+              {/* Project */}
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                  <CountUp start={0} end={20} duration={5} />+
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.5] max-w-[100px]">Finished projects</div>
+              </div>
             </div>
-          </div>
+          </motion.div>
         </div>
+
+        {/* ----------------------- */}
 
         {/* 2ed section */}
         <div className="flex flex-col w-full xl:max-w-[55%] h-[480px]">
           {/* about data title */}
+
           <div className="flex gap-x-8 xl:gap-x-12 text-white mx-auto xl:mx-0 mb-4">
             {aboutData.map((item, itemIndex) => {
               const { title } = item;
@@ -70,6 +97,7 @@ const About = () => {
               );
             })}
           </div>
+
           {/* about data details */}
           <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
             {aboutData[index].info.map((item, itemIndex) => {
