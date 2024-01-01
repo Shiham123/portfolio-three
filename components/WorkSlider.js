ef2da12471060ff534caebe344154b6b736483e4
Shiham123 <s36651786@gmail.com>
2024-01-01 22:27:04 +0600

    work section done with animation style and hover effect and slider

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -22,14 +22,30 @@ const WorkSlider = () => {
         const { images } = item;
         return (
           <SwiperSlide key={index}>
-            <div>
+            <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
               {images.map((item, secondIndex) => {
-                const { title, path } = item;
+                const { path } = item;
                 return (
-                  <div key={secondIndex}>
-                    <div>{title}</div>
-                    <div>
+                  <div key={secondIndex} className="relative rounded-lg overflow-hidden flex items-center justify-center group">
+                    <div className="flex items-center justify-center relative overflow-hidden group">
                       <Image src={path} width={500} height={300} alt="images" />
+                      {/* overlay gradient */}
+                      <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
+
+                      {/* title */}
+                      <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300 text-[13px] tracking-[0.2rem]">
+                        <div className="flex items-center gap-x-2 ">
+                          {/* title part one */}
+                          <div className="delay-100">Live Link</div>
+                          {/* title part two */}
+                          <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">Code Link</div>
+
+                          {/* icon */}
+                          <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200">
+                            <BsArrowRight />
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 );
