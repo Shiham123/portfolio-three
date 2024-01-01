e28fa8da52b9c07e790365b4ad17851d4f4091fb
Shiham123 <s36651786@gmail.com>
2024-01-01 20:35:32 +0600

    initail silde show but data not set

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper/modules';
+import { BsArrowRight } from 'react-icons/bs';
 import 'swiper/css/pagination';
 import 'swiper/css';
 
@@ -10,32 +11,22 @@ const WorkSlider = () => {
   const [slidePerScreen, setSlidePerScreen] = useState(3);
   const { slides } = workSlides;
 
+  // here are handle slides per screen resize
   useEffect(() => {
     const resizeSlides = () =>
       window.innerWidth >= 1200 ? setSlidePerScreen(3) : window.innerWidth >= 768 ? setSlidePerScreen(2) : setSlidePerScreen(1);
-
     resizeSlides();
     return () => window.removeEventListener('resize', resizeSlides);
   }, []);
 
+  // pagination condition
   const paginationCondition = { clickable: true };
+
   return (
     <Swiper pagination={paginationCondition} modules={[Pagination]} slidesPerView={slidePerScreen} className="h-[240px] sm:h-[340px]">
       {slides.map((item, index) => {
         const { images } = item;
-
-        images.map((item, index) => {
-          const { title, path } = item;
-          console.log(title, path);
-
-          return (
-            <SwiperSlide key={index}>
-              <div>
-                <div>{title}</div>
-              </div>
-            </SwiperSlide>
-          );
-        });
+        return <SwiperSlide key={index}>Slide</SwiperSlide>;
       })}
     </Swiper>
   );
