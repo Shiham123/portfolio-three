89bd46982893352be59df6f03e94bd8025bd1398
Shiham123 <s36651786@gmail.com>
2024-03-25 21:38:38 +0600

    updated home page content

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,8 @@ const Home = () => {
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           {/* title */}
           <motion.h1 variants={fadeIn('down', 0.2)} initial="hidden" animate="show" exit="hidden" className="h1">
-            Transform ideas <br /> Info <span className="text-accent">digital reality</span>
+            Shiham Bin Yousuf
+            <br /> <span className="text-accent">MERN Stack Developer</span>
           </motion.h1>
           {/* Subtitle */}
           <motion.p
@@ -29,8 +30,8 @@ const Home = () => {
             exit="hidden"
             className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16 lowercase"
           >
-            THis is next js portfolio. i make this for my technical skill growth purpose. i want to know react js for this purpose i am coming to make
-            this project from youtube.
+            While I am a MERN stack developer, I don&apos;t limit myself to just that. There are countless languages and full stacks out there, and I
+            am constantly exploring and learning new ones every day.
           </motion.p>
           {/* btn */}
           <div className="flex justify-center xl:hidden relative">
