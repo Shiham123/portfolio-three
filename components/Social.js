1dc7c356c92bc3f4c6f66733315fecaa1ab5339f
Shiham123 <s36651786@gmail.com>
2024-03-28 22:45:26 +0600

    some changes

--- components/Social.js
+++ components/Social.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import Link from 'next/link';
 
-import { RiFacebookLine, RiGithubLine, RiGoogleLine, RiInstagramLine, RiLinkedinLine, RiTwitterLine } from 'react-icons/ri';
+import { RiGithubLine, RiGoogleLine, RiLinkedinLine } from 'react-icons/ri';
+import { FaGoogleDrive } from 'react-icons/fa';
 
 const Social = () => {
   return (
     <div className="flex items-center gap-x-5 text-lg">
-      <Link href={'https://www.instagram.com/shihamusuf'} target="_blank" className="hover:text-accent transition-all duration-300">
-        <RiInstagramLine size={30} />
-      </Link>
-      <Link href={'https://www.facebook.com/fairoz.rahaman.1/'} target="_blank" className="hover:text-accent transition-all duration-300">
-        <RiFacebookLine size={30} />
-      </Link>
       <Link href={'https://github.com/Shiham123'} target="_blank" className="hover:text-accent transition-all duration-300">
         <RiGithubLine size={30} />
       </Link>
       <Link href={'mailto:shiham36651786@gmail.com'} className="hover:text-accent transition-all duration-300">
         <RiGoogleLine size={30} />
       </Link>
-      <Link href={'https://twitter.com/ShihamUsuf'} target="_blank" className="hover:text-accent transition-all duration-300">
-        <RiTwitterLine size={30} />
-      </Link>
       <Link href={'https://www.linkedin.com/in/shiham36651786'} target="_blank" className="hover:text-accent transition-all duration-300">
         <RiLinkedinLine size={30} />
+      </Link>
+      <Link
+        href={'https://drive.google.com/file/d/1vvVNPURGEdoin_8O-t57-EkAAlW0d5yI/view?usp=sharing'}
+        target="_blank"
+        className="hover:text-accent transition-all duration-300"
+      >
+        <FaGoogleDrive size={30} />
       </Link>
     </div>
   );
